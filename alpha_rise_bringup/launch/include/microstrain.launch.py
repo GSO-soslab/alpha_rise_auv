3257a59552c96a443ec7acdb7f23e36a05225604
tjsos <tony.jacob@uri.edu>
2025-05-19 14:45:51 -0400

    feat: tested for simulation

---
+++ alpha_rise_bringup/launch/include/microstrain.launch.py
@@ -0,0 +1,35 @@
+import os
+import yaml
+import pathlib
+from launch import LaunchDescription
+import launch.actions
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from launch.substitutions import EnvironmentVariable
+from launch.actions import DeclareLaunchArgument
+
+def generate_launch_description():
+    robot_name = 'alpha_rise'
+    robot_config = robot_name + '_config'
+
+    ld = LaunchDescription()
+
+    param_config = os.path.join(
+        get_package_share_directory(robot_config),
+        'config',
+        'microstrain.yaml'
+    )
+    
+
+    node = Node(
+        package='microstrain_inertial_driver',
+        executable='microstrain_inertial_driver_node',
+        name='microstrain_inertial_driver',
+        namespace="alpha_rise",
+        output='screen',
+        parameters=[param_config]        
+    )
+
+    ld.add_action(node)
+
+    return ld
---
+++ alpha_rise_bringup/launch/include/waterlinked_dvl.launch.py
@@ -0,0 +1,35 @@
+import os
+import yaml
+import pathlib
+from launch import LaunchDescription
+import launch.actions
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from launch.substitutions import EnvironmentVariable
+from launch.actions import DeclareLaunchArgument
+
+def generate_launch_description():
+    robot_name = 'alpha_rise'
+    robot_config = robot_name + '_config'
+
+    ld = LaunchDescription()
+
+    param_config = os.path.join(
+        get_package_share_directory(robot_config),
+        'config',
+        'waterlinked_dvl.yaml'
+    )
+    
+
+    node = Node(
+        package='waterlinked_dvl',
+        executable='waterlinked_dvl_tcp_node',
+        name='waterlinked_dvl',
+        namespace="alpha_rise",
+        output='screen',
+        parameters=[param_config]        
+    )
+
+    ld.add_action(node)
+
+    return ld
