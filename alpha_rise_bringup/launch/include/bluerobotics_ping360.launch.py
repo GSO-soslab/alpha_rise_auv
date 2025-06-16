9019355974eb8fd4c37ef1d46c91fa2c905c57ec
mzhouURI <mzhou@uri.edu>
2025-06-16 19:30:13 +0000

    changes from field test

--- alpha_rise_bringup/launch/include/bluerobotics_ping360.launch.py
+++ alpha_rise_bringup/launch/include/bluerobotics_ping360.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
         get_package_share_directory(robot_bringup),
         'config',
         'sensors',
-        'bluerobotics_ping360.yaml'
+        'bluerobotcs_ping360.yaml'
     )
 
     node = Node(
