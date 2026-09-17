666281764da167fa3949d5025f9473e1eddec684
tjsos <tony.jacob@uri.edu>
2026-09-17 15:06:33 -0400

    feat: updated sim world

--- alpha_rise_bringup/launch/include/simulation.launch.py
+++ alpha_rise_bringup/launch/include/simulation.launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
     robot_name = 'alpha_rise'
     robot_bringup = robot_name + '_bringup'
     
-    sim_world = 'ocean_rise.scn'
+    sim_world = 'ocean.scn'
 
     world_of_stonefish_dir = get_package_share_directory('world_of_stonefish')
 
